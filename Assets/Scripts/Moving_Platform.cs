d5dfee286d5ac25a9cf56b9aea7c7b36cbcc777b
CrystalxBlade <satish141m@gmail.com>
2026-03-22 13:27:54 +0530

    point

--- Assets/Scripts/Moving_Platform.cs
+++ Assets/Scripts/Moving_Platform.cs
@@ -5,12 +5,28 @@ public class Moving_Platform : MonoBehaviour
     [SerializeField] Transform pointA, pointB;
     [SerializeField] float _moveSpeed;
     Transform target;
+    [SerializeField] float waitTime;
+    float waitTimer;
+    bool isWaiting;
     void Start()
     {
         target = pointB;
     }
     void Update()
     {
+        if(isWaiting)
+        {
+            waitTimer += Time.deltaTime;
+
+            if(waitTimer >= waitTime)
+            {
+                isWaiting = false;
+                waitTimer = 0;
+
+                target =  target ==  pointA ? pointB : pointA;
+            }
+        }
+
         transform.position = Vector3.MoveTowards(transform.position, target.position, _moveSpeed * Time.deltaTime);
 
         if(Vector3.Distance(transform.position, target.position) < 0.01f)
