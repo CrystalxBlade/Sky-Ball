9a667cf84071d9241227887934110b8634c55b0e
CrystalxBlade <satish141m@gmail.com>
2026-03-25 00:13:02 +0530

    bridge

--- Assets/Scripts/Home.cs
+++ Assets/Scripts/Home.cs
@@ -2,5 +2,16 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class Home : MonoBehaviour
 {
-    
+    public void Play()
+    {
+        
+    }
+    public void Levels()
+    {
+        
+    }
+    public void Quit()
+    {
+        Application.Quit();
+    }
 }
