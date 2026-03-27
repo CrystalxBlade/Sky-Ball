049cb69cca03d5256099bd18e23daa952b1d7604
CrystalxBlade <satish141m@gmail.com>
2026-03-27 23:18:08 +0530

    load

--- Assets/Scripts/Level_Select.cs
+++ Assets/Scripts/Level_Select.cs
@@ -1,6 +1,11 @@
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class Level_Select : MonoBehaviour
 {
-    
+    [SerializeField] float lvl;
+
+    public void LoadLvl(int lvl)
+    {
+        SceneManager.LoadScene(lvl);
+    }
 }
