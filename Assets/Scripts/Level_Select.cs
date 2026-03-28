f71d2bab85e86ad0e464087252039d38cf830298
CrystalxBlade <satish141m@gmail.com>
2026-03-28 12:22:16 +0530

    newScene

--- Assets/Scripts/Level_Select.cs
+++ Assets/Scripts/Level_Select.cs
@@ -1,9 +1,21 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 public class Level_Select : MonoBehaviour
 {
     [SerializeField] int lvlNo;
 
+    void Start()
+    {
+        if(lvlNo <= PlayerPrefs.GetInt("Level", 1))
+        {
+            gameObject.GetComponent<Button>().interactable = true;
+        }
+        else
+        {
+            gameObject.GetComponent<Button>().interactable = false;
+        }
+    }
     public void LoadLvl()
     {
         SceneManager.LoadScene(lvlNo);
