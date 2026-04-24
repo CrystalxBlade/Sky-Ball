9260ada4c3de2678ad2d350aba5ba6220f2e7ebf
CrystalxBlade <satish141m@gmail.com>
2026-04-24 15:11:18 +0530

    Credit

--- Assets/Scripts/CreditEscape.cs
+++ Assets/Scripts/CreditEscape.cs
@@ -1,16 +1,14 @@
+using Unity.VectorGraphics;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CreditEscape : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            SceneManager.LoadScene(1);
+        }
     }
 }
