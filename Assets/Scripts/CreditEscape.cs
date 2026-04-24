a39b72eaa4a0f26660b5874ccee635a985500888
CrystalxBlade <satish141m@gmail.com>
2026-04-24 17:00:29 +0530

    newEscape

--- Assets/Scripts/CreditEscape.cs
+++ Assets/Scripts/CreditEscape.cs
@@ -4,11 +4,29 @@ using UnityEngine.SceneManagement;
 
 public class CreditEscape : MonoBehaviour
 {
+    bool cursorRequest;
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = false;
+        cursorRequest = false;
+    }
     void Update()
     {
         if(Input.GetKeyDown(KeyCode.Escape))
         {
-            SceneManager.LoadScene(1);
+            cursorRequest = !cursorRequest;
+            
+            if(cursorRequest)
+            {
+                Cursor.lockState = CursorLockMode.None;
+                Cursor.visible = true;
+            }
+            else
+            {
+                Cursor.lockState = CursorLockMode.Locked;
+                Cursor.visible = false;
+            }
         }
     }
 }
